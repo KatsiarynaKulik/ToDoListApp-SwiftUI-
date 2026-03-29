c7cc461d1db3b56f1fa115359a3fe66ecf2e6749
Katsiaryna Kulik <kulik_03_10@icloud.com>
2026-03-30 00:02:39 +0300

    Initial Commit

---
+++ ToDoListApp(SwiftUI)/ToDoListApp_SwiftUI_App.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp_SwiftUI_App.swift
+//  ToDoListApp(SwiftUI)
+//
+//  Created by Екатерина  on 30.03.26.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp_SwiftUI_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
