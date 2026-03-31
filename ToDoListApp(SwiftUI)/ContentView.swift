a9a21dba8bbbcf977851169d8c4dd55bc6d45482
Katsiaryna Kulik <kulik_03_10@icloud.com>
2026-03-31 16:48:56 +0300

    Add ContentView.

--- ToDoListApp(SwiftUI)/ContentView.swift
+++ ToDoListApp(SwiftUI)/ContentView.swift
@@ -6,16 +6,80 @@
 //
 
 import SwiftUI
+import Observation
+
+struct Task: Identifiable {
+    let id = UUID()
+    var name: String
+    var isCompleted: Bool
+}
+
+@Observable
+class TaskViewModel {
+    var tasks: [Task] = []
+    func addTask(title: String) {
+        tasks.append(Task(name: title, isCompleted: false))
+    }
+
+    func toggleCompletion(for task: Task) {
+        if let index = tasks.firstIndex(where: { $0.id == task.id }) {
+            tasks[index].isCompleted.toggle()
+        }
+    }
+
+    func deleteTask(at offsets: IndexSet) {
+        tasks.remove(atOffsets: offsets)
+
+    }
+}
 
 struct ContentView: View {
+    @State private var viewModel = TaskViewModel()
+    @State private var newTaskTitle: String = ""
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            VStack {
+                HStack{
+                    TextField("Enter new Task", text: $newTaskTitle)
+                        .textFieldStyle(.roundedBorder)
+
+                    Button {
+                        guard !newTaskTitle.isEmpty else { return }
+                        viewModel.addTask(title: newTaskTitle)
+                        newTaskTitle = ""
+                    } label: {
+                        Image(systemName: "plus.circle.fill")
+                            .font(.title2)
+                    }
+                    .buttonStyle(.borderless)
+                    .tint(.blue)
+                }
+                .padding()
+
+                List {
+                    ForEach(viewModel.tasks) { task in
+                        HStack {
+                            Text(task.name)
+                                .strikethrough(task.isCompleted)
+                                .foregroundStyle(task.isCompleted ? .gray : .primary)
+                            Spacer()
+                            Button {
+                                viewModel.toggleCompletion(for: task)
+                            } label: {
+                                Image(systemName: task.isCompleted ? "checkmark.square.fill" : "circle")
+                            }
+                            .buttonStyle(.borderless)
+                            .tint(task.isCompleted ? .green : .gray)
+                        }
+                    }
+                    .onDelete(perform: viewModel.deleteTask)
+                }
+            }
+            .navigationTitle("ToDo List")
+            .toolbar {
+                EditButton()
+            }
         }
-        .padding()
     }
 }
 
